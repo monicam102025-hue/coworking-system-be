1d4adbc0bfb536997a598b4a2dc2dd5dce3f1ebd
johanpina <johanpina1420@gmail.com>
2025-01-28 14:11:21 -0500

    adding CORS

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.routers import users, auth, bookings, spaces
 from app.core.database import create_tables
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todos los orígenes
+    allow_credentials=True,  # Permitir cookies/autenticación
+    allow_methods=["*"],  # Permitir todos los métodos HTTP (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
 create_tables()
 
